1ea5cdf26a5901d81c126327017c06f0a6ae1b6e
approxii <118077669+approxii@users.noreply.github.com>
2024-10-15 13:05:51 +0300

    Delete core directory

--- core/api/router/excel/depends.py
+++
@@ -1,5 +0,0 @@
-from core.services.excel import ExcelService as Service
-
-
-async def get_service() -> Service:
-    return Service()
--- core/api/router/word/depends.py
+++
@@ -1,5 +0,0 @@
-from core.services.word import WordService as Service
-
-
-async def get_service() -> Service:
-    return Service()
